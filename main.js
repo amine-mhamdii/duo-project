d8f3e82de6ab42b29103b2256631808f319e0b70
amine-mhamdii <medaminemhamdi@yahoo.com>
2020-07-18 09:25:42 +0100

    .

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-$(function(){
-	$("#btn").on("click",function(){
-		$("#log").hide()
-	})
-});
+$("#btn").on("click",function () {
+	localStorage.setItem("name",$("#name").val())
+	localStorage.setItem("pass",$("#pass").val())
+	$("#pass").val("")
+	$("#name").val("")
+
+})
+$("#btnlog").on("click",function(){
+	if(localStorage.getItem("name")===$("#name").val()&& $("#pass").val()===localStorage.getItem("pass")){
+		window.open("index2.html")
+	}else{
+		alert("wrong")
+	}
+})
